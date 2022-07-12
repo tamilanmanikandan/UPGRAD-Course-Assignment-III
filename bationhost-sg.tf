7df2a114d33a3c4c3617335f7a6729d1e0b8b8af
tamilanmanikandan <manikandanprabacse@gmail.com>
2022-07-12 21:57:30 +0530

    Update bationhost-sg.tf

--- bationhost-sg.tf
+++ bationhost-sg.tf
@@ -5,12 +5,10 @@ resource "aws_security_group" "ingress-all-test" {
 name = "bastion-host-sg"
 vpc_id = module.vpc.vpc_id
 ingress {
-    cidr_blocks = [
-      "${chomp(data.http.icanhazip.body)}/32"
-    ]
-from_port = 22
+    from_port = 22
     to_port = 22
     protocol = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 // Terraform removes the default rule
   egress {
