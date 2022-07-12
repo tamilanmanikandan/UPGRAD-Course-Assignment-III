42fbef9915081616a68cba7faebc31e047c1063e
tamilanmanikandan <manikandanprabacse@gmail.com>
2022-07-12 21:58:40 +0530

    Update bationhost-sg.tf

--- bationhost-sg.tf
+++ bationhost-sg.tf
@@ -5,6 +5,9 @@ resource "aws_security_group" "ingress-all-test" {
 name = "bastion-host-sg"
 vpc_id = module.vpc.vpc_id
 ingress {
+#    cidr_blocks = [
+#       "${chomp(data.http.icanhazip.body)}/32"
+#     ]
     from_port = 22
     to_port = 22
     protocol = "tcp"
